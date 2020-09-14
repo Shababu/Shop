51055616540e76f84aa82739d9e0ebb13e903806
Александр <skinnyshabab@gmail.com>
2020-09-14 16:20:10 +0300

    Главный класс команды

---
+++ Shop/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Shop.Infrastructure.Commands.Base;
+
+namespace Shop.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand(Action<object> Ececute, Func<object, bool> CanExecute = null)
+        {
+            if(_Execute != null)
+            {
+                _Execute = Execute;
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(Execute));
+            }
+            _CanExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object parameter) => _Execute?.Invoke(parameter);
+    }
+}
